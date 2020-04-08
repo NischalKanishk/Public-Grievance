d5870a71fe67fec09143832aa684e9d36f5a5e3d
prithvigg <prithvigudodagi29@gmail.com>
2020-04-08 12:34:15 +0530

    database info added

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,10 +1,11 @@
 //Dependencies
+require('custom-env').env()
 const express = require('express')
 const router  = express.Router()
 const bcrypt = require('bcryptjs')
-
-
-
+const ibmdb = require('ibm_db');
+//required to connect to database
+// let connStr = "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=rmw15324;PWD=md1tz7z-8x6p6h5d;";
 
 //Routes
 router.get('/login', (req, res)=>{
@@ -12,8 +13,26 @@ router.get('/login', (req, res)=>{
 })
 
 router.post('/login', (req, res)=>{
-   const user =  req.body.username
+    const user =  req.body.username
    const pass =  req.body.password
+   // connection to database start required 
+    // ibmdb.open(connStr,function(err,conn){
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         var str ="INSERT INTO RMW15324.USERS (username , password) VALUES ('"+user+"' , '"+pass+"');";
+    //         conn.query(str,function(err,data){
+    //             if(err){
+    //                 console.log(err);
+    //             }else{
+    //                 conn.close(function(){
+    //                     console.log("connection to database is closed");
+    //                 });
+    //             }
+    //         });
+    //     }
+    // });
+   
    if(user === 'admin' && pass ==='admin' ){
         res.redirect('/panel')
         console.log('User logged in')
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Dependencies
+require('custom-env').env()
 const express = require('express')
 const app     = express()
 const PORT    = process.env.PORT || 3000
 const bodyParser = require('body-parser')
+
+
 
 //Middlewares
 app.use(express.json({extended: false}))
@@ -18,6 +21,5 @@ app.get('/', (req, res)=>{
 // app.use('/api/search', require('./routes/api/search'))
 // app.use('/api/user', require('./routes/api/user'))
 app.use('/admin', require('./routes/api/admin'))
-
 //Server Start
 app.listen(PORT, ()=> console.log(`Server started at ${PORT}`))
