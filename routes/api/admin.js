ed90c2b51fc754eb661a1bed0a35ca643eaba622
prithvigg <prithvigudodagi29@gmail.com>
2020-04-08 09:27:20 +0530

    prithi,s first commit

---
+++ routes/api/admin.js
@@ -0,0 +1,28 @@
+//Dependencies
+const express = require('express')
+const router  = express.Router()
+const bcrypt = require('bcryptjs')
+
+
+
+
+//Routes
+router.get('/login', (req, res)=>{
+    res.render('login.ejs')
+})
+
+router.post('/login', (req, res)=>{
+   const user =  req.body.username
+   const pass =  req.body.password
+   if(user === 'admin' && pass ==='admin' ){
+        res.redirect('/panel')
+        console.log('User logged in')
+   }
+   else{
+       res.redirect('back')
+       console.log('Wrong creds')
+   }
+})
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,24 @@
+//Dependencies
+const express = require('express')
+const app     = express()
+const PORT    = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+
+//Middlewares
+app.use(express.json({extended: false}))
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+//Home routes
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
+//hello
+//Routes
+// app.use('/api/search', require('./routes/api/search'))
+// app.use('/api/user', require('./routes/api/user'))
+app.use('/admin', require('./routes/api/admin'))
+
+//Server Start
+app.listen(PORT, ()=> console.log(`Server started at ${PORT}`))
