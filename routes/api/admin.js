00f72181e861a58e5e098a78030891c15cb17bbb
Nischal Kanishk <nischalkanishk@gmail.com>
2020-04-10 11:47:24 +0530

    added panel

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -4,6 +4,9 @@ const router  = express.Router()
 const bcrypt = require('bcryptjs')
 const ibmdb = require('ibm_db');
 
+let connStr = "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-08.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=jfk88129;PWD=kz2hrcjn8tw^j1hz;";
+
+
 //Routes
 router.get('/login', (req, res)=>{
     res.render('login.ejs')
@@ -13,7 +16,7 @@ router.post('/login', (req, res)=>{
     const user =  req.body.username
    const pass =  req.body.password
  if(user === 'admin' && pass ==='admin' ){
-        res.redirect('/panel')
+        res.redirect('/admin/panel')
         console.log('User logged in')
    }
    else{
@@ -23,7 +26,23 @@ router.post('/login', (req, res)=>{
 })
 
 router.get('/panel', (req, res)=>{
-    res.render('panel')
+    ibmdb.open(connStr,function(err,conn){
+        if(err){
+            console.log(err);
+        }
+        console.log("connection successful");
+            var inputdata = "SELECT * FROM JFK88129.EDUCATION , JFK88129.AGRICULTURE , JFK88129.HEALTH;"
+            conn.query(inputdata,(err,data)=>{
+                if(err){
+                    console.log(err.message);
+                }else{
+                    conn.close(() => {
+                        console.log(data)
+                        res.render("panel",{info : data});               
+                    });
+                }
+            });
+    });
 })
 
 
